5e5b867db5ffe285d00e6cc3a039e1e90fa8001f
pratikbhor95 <pratikbhor95@gmail.com>
2025-09-04 15:03:28 +0100

    middleware

--- app/main.py
+++ app/main.py
@@ -3,11 +3,22 @@ from app import models
 from app.database import engine
 from app.routers import users, post, auth , vote
 from app.config import settings
+from fastapi.middleware.cors import CORSMiddleware
 
 
-models.Base.metadata.create_all(bind = engine)
+# models.Base.metadata.create_all(bind = engine)
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=origins,
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"],
+)
 
 app.include_router(post.router)
 app.include_router(users.router)
