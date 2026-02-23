a371660e52d77be490a4f7660f9f95b2ec070210
Pratikbhor95 <pratikbhor95@gmail.com>
2026-02-24 00:58:20 +0530

    changes in postgres version and alembic

--- alembic/env.py
+++ alembic/env.py
@@ -5,19 +5,21 @@ from sqlalchemy import pool
 
 from alembic import context
 from app.models import Base
-from app.config import settings
+from app.database import SQLALCHEMY_DATABASE_URL
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option("sqlalchemy.url" , f'postgresql+psycopg2://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}')
-
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
+config.set_main_option(
+    "sqlalchemy.url", 
+    SQLALCHEMY_DATABASE_URL
+)
 
 # add your model's MetaData object here
 # for 'autogenerate' support
--- app/config.py
+++ app/config.py
@@ -1,7 +1,5 @@
 from pydantic_settings import BaseSettings
 
-
-
 class Settings(BaseSettings):
   database_hostname : str
   database_port : str
